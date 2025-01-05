5f6c10ab4419348824ca4d0e7f90cf97dc6a45b3
Josh Patterson <Josh@NTDLS.com>
2025-01-05 18:41:36 -0500

    Public polymorphic deserialization.

--- NTDLS.CatMQ.Shared/CMqUnboxing.cs
+++ NTDLS.CatMQ.Shared/CMqUnboxing.cs
@@ -4,7 +4,7 @@ using System.Reflection;
 
 namespace NTDLS.CatMQ.Shared
 {
-    internal static class CMqUnboxing
+    public static class CMqUnboxing
     {
         private static readonly JsonSerializerSettings _typeNameHandlingAll = new()
         {
@@ -16,15 +16,58 @@ namespace NTDLS.CatMQ.Shared
         /// <summary>
         /// Deserialization function called from MessageDeliveryQuery via reflection.
         /// </summary>
-        public static T? MqDeserializeToObject<T>(string json)
+        internal static T? MqDeserializeToObject<T>(string json)
             => JsonConvert.DeserializeObject<T>(json, _typeNameHandlingAll);
+
+        /// <summary>
+        /// Deserializes json to an object of the given type string.
+        /// </summary>
+        public static object PolymorphicDeserialize(string assemblyQualifiedTypeName, string objectJson)
+        {
+            string cacheKey = $"{assemblyQualifiedTypeName}";
+
+            var genericToObjectMethod = _reflectionCache.Use((o) =>
+            {
+                if (o.TryGetValue(cacheKey, out var method))
+                {
+                    return method;
+                }
+                return null;
+            });
+
+            object? deserializedMessage = null;
+
+            if (genericToObjectMethod != null) //Reflection cache hit.
+            {
+                //Call the generic deserialization:
+                deserializedMessage = genericToObjectMethod.Invoke(null, [objectJson])
+                    ?? throw new Exception($"Extraction message can not be null.");
+            }
+            else
+            {
+                var genericType = Type.GetType(assemblyQualifiedTypeName)
+                    ?? throw new Exception($"Unknown extraction message type {assemblyQualifiedTypeName}.");
+
+                var toObjectMethod = typeof(CMqUnboxing).GetMethod("MqDeserializeToObject")
+                        ?? throw new Exception($"Could not resolve MqDeserializeToObject().");
+
+                genericToObjectMethod = toObjectMethod.MakeGenericMethod(genericType);
+
+                _reflectionCache.Use((o) => o.TryAdd(cacheKey, genericToObjectMethod));
+
+                //Call the generic deserialization:
+                deserializedMessage = genericToObjectMethod.Invoke(null, [objectJson])
+                    ?? throw new Exception($"Extraction message can not be null.");
+            }
+
+            return deserializedMessage;
+        }
 
         /// <summary>
         /// Deserializes the received message to its original type.
         /// </summary>
-        public static ICMqMessage Unbox(CMqReceivedMessage message)
+        internal static ICMqMessage Unbox(CMqReceivedMessage message)
         {
-
             string cacheKey = $"{message.ObjectType}";
 
             var genericToObjectMethod = _reflectionCache.Use((o) =>
